15d74a7eb813ddcfced793ba561867dd1b40d8a9
Morgan Fine-Morris <mfinemorris@icloud.com>
2015-08-18 16:20:04 -0700

    add the site navigation via navigation.js script. Will probably change the method later, but this works for now.

---
+++ navigation.js
@@ -0,0 +1,88 @@
+/*
+document.getElementByTagName("head").innerHTML=
+"<meta charset='utf-8'/>"+
+"<title>Wilson Lab</title>"+
+""+
+"<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'></link>"+
+"<link href='http://fonts.googleapis.com/css?family=Play:400,700' rel='stylesheet' type='text/css'></link>"+
+"<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css' media='all' rel='stylesheet' type='text/css'>"+
+"<link rel='stylesheet' href='../wilson_site.css'></link>"+
+""+
+"<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js'></script>";
+*/
+
+document.getElementById("menu-frame").innerHTML=
+"<h1 id='lab-title'>"+
+"    <a href='../Home/main_page.html'>"+
+"    Wilson <br> Lab"+
+"    </a>"+
+"</h1>"+
+""+
+"<div id='menu'>"+
+"    <ul>"+
+"    "+
+"        <li>"+
+"            <div class='menu-item'>"+
+"                <a href='../Home/main_page.html'>"+
+"                    <i class='fa fa-home menu-icon'></i>"+
+"                    <span>Home</span>"+
+"                </a>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"        <li>"+
+"            <div class='menu-item'>"+
+"                <a href='../Research/research_page.html'>"+
+"                    <i class='fa fa-flask menu-icon'></i>"+
+"                    <span>Research</span>"+
+"                </a>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"        <li>"+
+"            <div class='menu-item'>"+
+"                <a href='../Publications/publications_page.html'>"+
+"                    <i class='fa fa-list-alt menu-icon'></i>"+
+"                    <span>Publications</span>"+
+"                </a>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"        <li>"+
+"            <div class='menu-item'>"+
+"                <a href='../Programming/programming_page.html'>"+
+"                    <i class='fa fa-desktop menu-icon'></i>"+
+"                    <span>Programming</span>"+
+"                </a>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"        <li>"+
+"            <div class='menu-item'>"+
+"                <a href='../People/people_page.html'>"+
+"                    <i class='fa fa-group menu-icon'></i>"+
+"                    <span>People</span>"+
+"                </a>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"        <li>"+
+"            <div class='menu-item' id = 'contact-block'>"+
+"                <span id='contact-info-title'>Contact Info</span>"+
+"                <img src='../SitePictures/Loma_linda_university_logo_cropped.png' alt='Loma Linda University Logo' id='uni-logo'>"+
+"                <ul id='contact-info'>"+
+"                    <li>Division of Physiology</li>"+
+"                    <li>School of Medicine</li>"+
+"                    <li>Loma Linda University</li>"+
+"                    <li>Loma Linda, CA 92350</li>"+
+"                    <li>U.S.A</li>"+
+"                    "+
+"                    <li><b>Phone:</b> (909) 651-5895</li>"+
+"                    <li><b>Fax:</b> (909) 558-4029</li>"+
+"                    <li><b>E-mail:</b> cgwilson@llu.edu</li>"+
+"                </ul>"+
+"            </div>"+
+"        </li>"+
+"        "+
+"    </ul>"+
+"</div>   ";
